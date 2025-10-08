57cb79e60f8af02c525a3d03e29a3c8fd700b8ee
Shovon96 <fakhruddinshovon@gmail.com>
2025-10-08 19:27:45 +0600

    completed the skills section

--- portfolio-frontend/src/components/home/skills/FrontendSkills.tsx
+++ portfolio-frontend/src/components/home/skills/FrontendSkills.tsx
@@ -1,24 +1,37 @@
-
 export default async function FrontendSkills() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/skills`, {
-        cache: "no-store"
+        cache: "no-store",
     });
     const skills = await res.json();
-    return (
-        <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4  gap-6 justify-center max-w-7xl mx-auto my-10">
-            {skills.map((skill: any) => (
-                <div key={skill.id} className="px-8 py-3 flex flex-col items-center gap-4 bg-gray-700 rounded-lg shadow-md shadow-primary border border-primary hover:scale-105 hover:shadow-lg transition-all duration-300">
-                    <img className="w-12 h-12" src={skill.imageUrl} alt={skill.title} />
-                    <p className="text-lg font-bold" style={{
-                        WebkitTextStrokeWidth: "1px",
-                        WebkitTextStrokeColor: "#e643a7",
-                        WebkitTextFillColor: "white",
-                    }}>
-                        {skill.title}
-                    </p>
-                </div>
-            ))}
-        </div>
 
-    )
+    const frontendSkills = skills?.filter((skill: any) => skill.skillType === "Frontend");
+
+    return (
+        <div className="grid grid-cols-2 lg:grid-cols-7 md:grid-cols-4 gap-6 justify-center max-w-7xl mx-auto my-10 px-4">
+            {frontendSkills && frontendSkills.length > 0 ? (
+                frontendSkills.map((skill: any) => (
+                    <div
+                        key={skill.id}
+                        className="px-8 py-3 flex flex-col items-center gap-4 bg-gray-700 rounded-lg shadow-md shadow-primary border border-primary hover:scale-105 hover:shadow-lg transition-all duration-300"
+                    >
+                        <img className="w-12 h-12" src={skill.imageUrl} alt={skill.title} />
+                        <p
+                            className="text-lg font-bold"
+                            style={{
+                                WebkitTextStrokeWidth: "1px",
+                                WebkitTextStrokeColor: "#e643a7",
+                                WebkitTextFillColor: "white",
+                            }}
+                        >
+                            {skill.title}
+                        </p>
+                    </div>
+                ))
+            ) : (
+                <p className="text-2xl font-bold text-center col-span-full">
+                    No Frontend Skills Found
+                </p>
+            )}
+        </div>
+    );
 }
--- portfolio-frontend/src/components/home/skills/Skills.tsx
+++ portfolio-frontend/src/components/home/skills/Skills.tsx
@@ -1,19 +1,30 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import FrontendSkills from "./FrontendSkills"
+import BackendSkills from "./BackendSkills"
+import SectionTitle from "@/components/shared/SectionTitle"
 
 export default function Skills() {
     return (
-        <Tabs defaultValue="frontend" className="max-w-7xl flex flex-col items-center justify-center mx-auto my-10">
-            <TabsList>
-                <TabsTrigger value="frontend">Frontend Skills</TabsTrigger>
-                <TabsTrigger value="backend">Backend Skills</TabsTrigger>
-            </TabsList>
-            <TabsContent value="frontend">
-                <FrontendSkills />
-            </TabsContent>
-            <TabsContent value="backend">
-                <FrontendSkills />
-            </TabsContent>
-        </Tabs>
+        <>
+            <div className="flex items-center justify-center gap-3 my-12">
+                <span className="h-[4px] mt-2 w-10 sm:w-16 md:w-42 bg-primary"></span>
+                <SectionTitle title1="Technical" />
+                <span className="text-4xl md:text-5xl font-extrabold text-primary">Me</span>
+                <span className="h-[4px] mt-2 w-10 sm:w-16 md:w-42 bg-primary"></span>
+            </div>
+
+            <Tabs defaultValue="frontend" className="max-w-7xl px-4 flex flex-col items-center justify-center mx-auto my-10">
+                <TabsList className="py-5">
+                    <TabsTrigger className="px-5 py-4" value="frontend">Frontend Skills</TabsTrigger>
+                    <TabsTrigger className="px-5 py-4" value="backend">Backend Skills</TabsTrigger>
+                </TabsList>
+                <TabsContent value="frontend">
+                    <FrontendSkills />
+                </TabsContent>
+                <TabsContent value="backend">
+                    <BackendSkills />
+                </TabsContent>
+            </Tabs>
+        </>
     )
 }
