49a183e885cbceafc5b73edb883eab14a3e4a986
Shovon96 <fakhruddinshovon@gmail.com>
2025-10-14 06:49:20 +0600

    add: delete project

--- portfolio-frontend/src/components/shared/ProjectCard.tsx
+++ portfolio-frontend/src/components/shared/ProjectCard.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import EditProjectModal from "@/app/(dashboard)/dashboard/manage-project/EditProjectModal";
 import { Trash2 } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 
 export interface IProjectCard {
@@ -15,7 +19,32 @@ export interface IProjectCard {
 
 
 export default function ProjectCard({ project, admin }: { project: IProjectCard, admin?: string }) {
+
     const { title, description, imageUrl, demoUrlFrontend, sourceCodeFrontend, sourceCodebackend, id } = project;
+
+    const router = useRouter();
+
+    // delete Project
+    const handleDelete = async (id: string) => {
+        try {
+            const confirmDelete = confirm("Are you sure you want to delete this project?");
+            if (!confirmDelete) return;
+
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/project/${id}`, {
+                method: "DELETE",
+                credentials: "include",
+            });
+
+            if (!res.ok) {
+                const errorData = await res.json();
+                throw new Error(errorData.message || "Failed to delete project");
+            }
+            toast.success("Project deleted successfully!");
+            router.refresh();
+        } catch (error: any) {
+            toast.error(error.message || "Something went wrong while deleting");
+        }
+    };
 
     return (
         <div className="w-[325px] md:w-[350px] h-fit rounded-md border border-[#470742] overflow-hidden transition duration-500 hover:scale-[1.04] bg-[#470742]">
@@ -34,9 +63,9 @@ export default function ProjectCard({ project, admin }: { project: IProjectCard,
             </Link>
             {admin === "Admin" ? (
                 <div className="flex justify-between items-center px-5 py-1 rounded-b-md bg-[#f1e5ff] h-14">
-                    <EditProjectModal project={project}/>
-                    <button className="text-sm flex items-center justify-center gap-1 font-semibold text-gray-900 border px-4 py-2 rounded cursor-pointer bg-red-400 hover:bg-red-500">
-                        <Trash2 className="h-6 w-4" /> Delete Blog
+                    <EditProjectModal project={project} />
+                    <button onClick={() => handleDelete(id)} className="text-sm flex items-center justify-center gap-1 font-semibold text-gray-900 border px-4 py-2 rounded cursor-pointer bg-red-400 hover:bg-red-500">
+                        <Trash2 className="h-6 w-4" /> Delete Project
                     </button>
                 </div>
             ) :
